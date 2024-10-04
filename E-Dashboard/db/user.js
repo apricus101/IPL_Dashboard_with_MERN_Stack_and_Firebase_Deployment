d67e18e58eefcdf58947133962e61c9d1a979732
apricus101 <119146402+apricus101@users.noreply.github.com>
2024-10-04 09:43:48 +0530

    Create user.js

---
+++ E-Dashboard/db/user.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+    name: String,
+    email: String,
+    age: String,
+    gender: String,
+    country: String,
+    password: String
+});
+
+module.exports = mongoose.model("users",userSchema);
